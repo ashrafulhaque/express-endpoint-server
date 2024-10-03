64da857ccc098c99492484117d02efb81790bd2e
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-10-03 23:56:51 +0600

    JSON data loaded and served on '\course' endpoint

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
 
+const courseData = require("./data/courseDetails.json");
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Ashraful's Express Endpoint Server is Running");
+});
+
+app.get("/course", (req, res) => {
+  res.send(courseData);
 });
 
 app.listen(port, () => {
