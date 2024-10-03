dfbe75ffa21aa3d5afbc7a130f5bbe632cb94d48
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-10-03 23:27:23 +0600

    Basic server setup is done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
- 
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Ashraful's Express Endpoint Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Ashraful's Express Endpoint Server is Running on Port ${port}`);
+});
