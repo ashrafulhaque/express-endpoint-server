ef8cc5d1a80d23892e0ec79c0231c2ebe4d060f0
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-10-07 01:42:35 +0600

    Added vercel build script

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// Programmer: Md. Ashraful haque
-// Description: A Node.js and Express.js backend serving product data through RESTful API endpoints
-
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 4000;
-
-const courseData = require("./data/courseDetails.json");
 
 app.use(cors());
+
+const courseData = require("./data/courseDetails.json");
 
 app.get("/", (req, res) => {
   res.send("Ashraful's Express Endpoint Server is Running");
@@ -20,21 +16,13 @@ app.get("/products", (req, res) => {
 });
 
 app.get("/products/:id", (req, res) => {
-  const id = req.params.id; // No need to parse as integer
+  const id = req.params.id;
   const course = courseData.find((c) => c._id === id);
-  console.log(id, course);
   if (!course) {
     return res.status(404).send({ error: "Course Not Found" });
   }
-
   res.send(course);
 });
 
-// Handle undefined routes (404 Not Found)
-app.use((req, res) => {
-  res.status(404).send({ error: "Endpoint Not Found" });
-});
-
-app.listen(port, () => {
-  console.log(`Ashraful's Express Endpoint Server is Running on Port ${port}`);
-});
+// Export the app to be used by Vercel
+module.exports = app;
