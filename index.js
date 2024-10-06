74704c0d62f0571842ebd0524cdd4386ba51d1c4
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-10-07 01:09:15 +0600

    Endpoints /product and /product/:id created and working.

--- index.js
+++ index.js
@@ -12,8 +12,19 @@ app.get("/", (req, res) => {
   res.send("Ashraful's Express Endpoint Server is Running");
 });
 
-app.get("/course", (req, res) => {
+app.get("/products", (req, res) => {
   res.send(courseData);
+});
+
+app.get("/products/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const course = courseData.find((c) => c._id === id);
+
+  if (!course) {
+    return res.status(404).send("Course Not Found");
+  }
+
+  res.send(course);
 });
 
 app.listen(port, () => {
